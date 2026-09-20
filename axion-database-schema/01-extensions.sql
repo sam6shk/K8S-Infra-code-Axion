4ab5dda58e683988dffdedae1aadaca41875a9e0
sam6shk <sameer.shaik6@outlook.com>
2026-09-20 11:21:46 +0530

    code push

--- axion-database-schema/01-extensions.sql
+++
@@ -1,5 +0,0 @@
--- ============================================
--- Axion v1 - Extensions
--- ============================================
--- Enable UUID generation via pgcrypto
-CREATE EXTENSION IF NOT EXISTS "pgcrypto";
